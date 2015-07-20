273342218ba40296fce349d18d8e7f886b1b503d
martinusbe <martinusbe@gmail.com>
2015-07-20 09:44:30 +0200

    common: build wallpaperpicker & v1.2
    Signed-off-by: martinusbe <martinusbe@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -97,7 +97,8 @@ PRODUCT_PACKAGES += \
     BluetoothExt \
     LockClock \
     DashClock \
-    CMFileManager
+    CMFileManager \
+    WallpaperPicker
 
 #    SlimFileManager removed until updated
 # Extra tools
@@ -161,7 +162,7 @@ vendor/tipsy/prebuilt/common/app/layersmanager.apk:system/app/layersmanager.apk
 # tipsyLP first version.
 PRODUCT_VERSION_MAJOR = 5.1.1
 PRODUCT_VERSION_MINOR = Intoxicated
-PRODUCT_VERSION_MAINTENANCE = v1.1
+PRODUCT_VERSION_MAINTENANCE = v1.2
 ifdef TIPSY_BUILD_EXTRA
     TIPSY_POSTFIX := -$(TIPSY_BUILD_EXTRA)
 endif

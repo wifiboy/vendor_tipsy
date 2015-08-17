6c9a827bb7f45fecfc54a1dfa06c18a789d9d04f
martinusbe <martinusbe@gmail.com>
2015-08-17 15:43:43 +0200

    devices add s4mini & p3110 to testing
    Signed-off-by: martinusbe <martinusbe@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -165,7 +165,7 @@ vendor/tipsy/prebuilt/common/app/layersmanager.apk:system/app/layersmanager.apk
 # tipsyLP first version.
 PRODUCT_VERSION_MAJOR = 5.1.1
 PRODUCT_VERSION_MINOR = Intoxicated
-PRODUCT_VERSION_MAINTENANCE = v1.5-testing
+PRODUCT_VERSION_MAINTENANCE = v1.5-rc
 ifdef TIPSY_BUILD_EXTRA
     TIPSY_POSTFIX := -$(TIPSY_BUILD_EXTRA)
 endif

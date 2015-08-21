c8127959b9b3dedfefe8222dd364a3a55d187756
martin coulon <martinusbe@gmail.com>
2015-08-21 16:31:35 +0200

    v1.6

--- config/common.mk
+++ config/common.mk
@@ -165,7 +165,7 @@ vendor/tipsy/prebuilt/common/app/layersmanager.apk:system/app/layersmanager.apk
 # tipsyLP first version.
 PRODUCT_VERSION_MAJOR = 5.1.1
 PRODUCT_VERSION_MINOR = Intoxicated
-PRODUCT_VERSION_MAINTENANCE = v1.5-rc
+PRODUCT_VERSION_MAINTENANCE = v1.6
 ifdef TIPSY_BUILD_EXTRA
     TIPSY_POSTFIX := -$(TIPSY_BUILD_EXTRA)
 endif

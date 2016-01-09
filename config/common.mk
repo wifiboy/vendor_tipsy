12f2282d0bd33b35255f5b7c50dd6d8e1142e543
Martinusbe <martinusbe@gmail.com>
2016-01-09 09:09:40 +0100

    updat versioning for 6.0.1--> Hoeagaerden
    Change-Id: I0049488d9052768d9042c3fbb6387955cc3426bd
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -155,9 +155,9 @@ vendor/tipsy/prebuilt/common/app/Layers_backup_restore.apk:system/app/Layers_bac
 
 # Versioning System
 # tipsyLP first version.
-PRODUCT_VERSION_MAJOR = 6.0
-PRODUCT_VERSION_MINOR = Picon-alpha
-PRODUCT_VERSION_MAINTENANCE = v3.0
+PRODUCT_VERSION_MAJOR = 6.0.1
+PRODUCT_VERSION_MINOR = Hoegaerden_alpha
+PRODUCT_VERSION_MAINTENANCE = v3.1
 ifdef TIPSY_BUILD_EXTRA
     TIPSY_POSTFIX := -$(TIPSY_BUILD_EXTRA)
 endif

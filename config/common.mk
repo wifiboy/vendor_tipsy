8ad6a04f1e974e8ea96db63fd57cfe36adaad2f7
Martinusbe <martinusbe@gmail.com>
2016-04-01 09:37:39 +0200

    3.5 Judas
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -164,8 +164,8 @@ vendor/tipsy/prebuilt/common/app/Layers_backup_restore.apk:system/app/Layers_bac
 # Versioning System
 # tipsyLP first version.
 PRODUCT_VERSION_MAJOR = 6.0.1
-PRODUCT_VERSION_MINOR = Hapkin
-PRODUCT_VERSION_MAINTENANCE = v3.4
+PRODUCT_VERSION_MINOR = Judas
+PRODUCT_VERSION_MAINTENANCE = v3.5
 ifdef TIPSY_BUILD_EXTRA
     TIPSY_POSTFIX := -$(TIPSY_BUILD_EXTRA)
 endif

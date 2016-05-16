c9369d44013d6849d9636f0401f7008da7407eb2
Alx31 <alex31.couderc@gmail.com>
2016-05-16 08:34:53 +0200

    Kirkstall_framboise it is for Pete =)

--- config/common.mk
+++ config/common.mk
@@ -169,7 +169,7 @@ vendor/tipsy/prebuilt/common/app/Layers_backup_restore.apk:system/app/Layers_bac
 # Versioning System
 # tipsyM first version.
 PRODUCT_VERSION_MAJOR = 6.0.1
-PRODUCT_VERSION_MINOR = OTAtest
+PRODUCT_VERSION_MINOR = Kirkstall_framboise
 PRODUCT_VERSION_MAINTENANCE = v4.0
 ifdef TIPSY_BUILD_EXTRA
     TIPSY_POSTFIX := $(TIPSY_BUILD_EXTRA)

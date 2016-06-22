d50c67729af1d9ae1d52c717659ad1ffeb5b273a
Martinusbe <martinusbe@gmail.com>
2016-06-22 11:21:27 +0200

    bootani: new one by Pete, 720p version
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -175,7 +175,7 @@ vendor/tipsy/prebuilt/common/app/Layers_backup_restore.apk:system/app/Layers_bac
 # tipsyM first version.
 PRODUCT_VERSION_MAJOR = 6.0.1
 PRODUCT_VERSION_MINOR = Geuze
-PRODUCT_VERSION_MAINTENANCE = v4.1
+PRODUCT_VERSION_MAINTENANCE = v4.2
 ifdef TIPSY_BUILD_EXTRA
     TIPSY_POSTFIX := $(TIPSY_BUILD_EXTRA)
 endif

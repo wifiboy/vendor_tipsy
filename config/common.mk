5c504849064cd7f9b5380e411439b99b215b81f3
Martinusbe <martinusbe@gmail.com>
2016-07-14 08:37:26 +0200

    forgot
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -171,7 +171,7 @@ vendor/tipsy/prebuilt/common/app/Layers_backup_restore.apk:system/app/Layers_bac
 # tipsyM first version.
 PRODUCT_VERSION_MAJOR = 6.0.1
 PRODUCT_VERSION_MINOR = OMS
-PRODUCT_VERSION_MAINTENANCE = v4.3
+PRODUCT_VERSION_MAINTENANCE = v4.4
 ifdef TIPSY_BUILD_EXTRA
     TIPSY_POSTFIX := $(TIPSY_BUILD_EXTRA)
 endif
